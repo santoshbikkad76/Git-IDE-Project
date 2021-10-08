796e86507a7095c4d461ec155e17354827f100fb
GanRaaJ <you@example.com>
2021-10-08 14:53:16 +0530

    IES-301: added another String variable and one method into main class

--- src/main/java/com/bikkadIt/GitFirstProjectApplication.java
+++ src/main/java/com/bikkadIt/GitFirstProjectApplication.java
@@ -5,11 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitFirstProjectApplication {
-
+String s1="Bikkad";
 	public static void main(String[] args) {
 		SpringApplication.run(GitFirstProjectApplication.class, args);
 
 public void doProcess(){
+
+}
+public void m1(){
 
 }
 	}
